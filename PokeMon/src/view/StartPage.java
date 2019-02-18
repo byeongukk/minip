94aa0f9eb76c066fe9aac73adcee19698ed6e288
junc <junc@DESKTOP-TCFGO3O>
2019-02-18 13:36:57 +0900

    메인 페이지 수정, 스타트 페이지수정

--- PokeMon/src/view/StartPage.java
+++ PokeMon/src/view/StartPage.java
@@ -1,10 +1,14 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import model.dao.UserDao;
@@ -15,59 +19,68 @@ public class StartPage extends JPanel{
 	private MainFrame mf;
 	private JPanel stp;
 	private User user;
-	
+	private Image mainPage = new ImageIcon("images/mainPage.gif").getImage();
+
 	public StartPage(MainFrame mf) {
 		this.mf = mf;
 		this.stp = this;
 		stp.setOpaque(false);
-	      stp.setBounds(0, 0, 1024, 768);
-	      this.setLayout(null);
-	      this.setBackground(Color.WHITE);
-	      
-	      JButton startbtn = new JButton("새로 시작");
-	      JButton nextPage = new JButton("모험 시작");
-	      startbtn.setSize(200,200);
-	      startbtn.setLocation(500,360);
-	      
-	      nextPage.setSize(200,200);
-	      nextPage.setLocation(300,200);
+		stp.setBounds(0, 0, 1024, 768);
+		this.setLayout(null);
+		this.setBackground(Color.WHITE);
+		Image jlb = new ImageIcon("images/mainPage.gif").getImage().getScaledInstance(1000, 720, 0);
+		JLabel jlb2 = new JLabel(new ImageIcon(jlb));
+		jlb2.setBounds(0, 0, 1000, 720);
+		
 
-	      
-	      startbtn.addMouseListener(new MouseAdapter() {
-	         
-	         @Override
-	         public void mousePressed(MouseEvent e) {
-	            
-	           
-	           
-	            /*mf.remove(stp);
+		
+		JButton startbtn = new JButton("새로 시작");
+		JButton nextPage = new JButton("모험 시작");
+		startbtn.setSize(150,50);
+		startbtn.setLocation(400,250);
+		startbtn.setFont(new Font("바탕체", Font.BOLD, 13));
+
+		nextPage.setSize(150,50);
+		nextPage.setLocation(500,250);
+
+
+		startbtn.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+
+
+
+				/*mf.remove(stp);
 	            m.setVisible(true);
 	            mf.requestFocus();
 	            ((Map) m).start();*/
 
-	         }
-	      });
-	         nextPage.addMouseListener(new MouseAdapter() {
-	        	 public void mousePressed(MouseEvent e) {
-	        		 UserDao ud = new UserDao("이름이름");
-	        		 //ud.getUserList().get(0).setuGold(500);
-	        		 ud.saveUser();
-	        		 stp.setVisible(false);
-	        		 mf.remove(stp);
-	        		 mf.add(new Map(mf, ud.getUserList().get(0)));
-	        		 mf.requestFocus();
-	        		
-	        		
-	        	 }
-	         });
-	     
-	      
-	         stp.add(startbtn);
-		      stp.add(nextPage);
-		      mf.add(this);
-	      
-	   }
-		
+			}
+		});
+		nextPage.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				UserDao ud = new UserDao("이름이름");
+				//ud.getUserList().get(0).setuGold(500);
+				ud.saveUser();
+				stp.setVisible(false);
+				mf.remove(stp);
+				mf.add(new Map(mf, ud.getUserList().get(0)));
+				mf.requestFocus();
+
+
+			}
+		});
+		stp.add(jlb2);
+		stp.add(startbtn);
+		stp.add(nextPage);
+		stp.setComponentZOrder(jlb2, 1);
+		stp.setComponentZOrder(startbtn, 0);
+		stp.setComponentZOrder(nextPage, 0);
+		mf.add(this);
+
 	}
-	
+
+}
+
 
